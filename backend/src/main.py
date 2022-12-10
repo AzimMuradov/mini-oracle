adac30f243b35c5a298061b6e9ce536d942d2765
Azim Muradov <azim.muradov.dev@gmail.com>
2022-12-11 00:50:24 +0300

    Remove `debug` option

--- backend/src/main.py
+++ backend/src/main.py
@@ -19,4 +19,4 @@ def get_answers():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
+    app.run(host='0.0.0.0', port=os.getenv("PORT", default=5000))
